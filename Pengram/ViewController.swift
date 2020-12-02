c88ee3505134c6ba3c94fff6a5c16134103ac27b
Abdulrahman <info@example.com>
2020-12-02 15:38:20 +0000

    Add sepia filter option

--- Pengram/ViewController.swift
+++ Pengram/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    // Define context for the button methods
+    let context = CIContext()
+    var original: UIImage?
+    
+    // Connect to the imageView
+    @IBOutlet var imageView: UIImageView!
+    // Connect to the nav button
+    @IBAction func choosePhoto()
+    {
+        // Check for permission to get an image
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary)
+        {
+            // Create a picker
+            let picker = UIImagePickerController()
+            // Delegate response to this class
+            picker.delegate = self
+            // Check the source
+            picker.sourceType = .photoLibrary
+            // Specify behaviour of navigation
+            navigationController?.present(picker, animated: true, completion: nil)
+        }
     }
-
-
+    
+    @IBAction func applySepia()
+    {
+        guard let original = original else {
+            return
+        }
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(0.5, forKey: kCIInputIntensityKey)
+        filter?.setValue(CIImage(image: original), forKey: kCIInputImageKey)
+        let output = filter?.outputImage
+        imageView.image = UIImage(cgImage: self.context.createCGImage(output!, from: output!.extent)!)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        navigationController?.dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = image
+            original = image
+        }
+    }
 }
 
